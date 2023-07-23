3d1ec09bd9ffded46fd1aa8a5fe320e051ddbbc6
bola2044 <priscilla9313@gmail.com>
2023-07-22 20:17:02 -0400

    modified and new

--- CureskinProject/features/environment.py
+++ CureskinProject/features/environment.py
@@ -1,66 +1,96 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.support.wait import WebDriverWait
-# from selenium.webdriver.firefox.service import Service
-# from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
+# from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 # from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+
 from app.application import Application
+from support.logger import logger
+
+def browser_init(context, test_name):
 
 
-def browser_init(context):
+# def browser_init(context):
     """
     :param context: Behave context
+    :param test_name: scenario.name
     """
+    ####### GOOGLE CHROME ######################
     # driver_path = ChromeDriverManager().install()
-    # service = Service(driver_path)
+    # service = ChromeService(executable_path='/Users/priscillao/QA/Cureskin/chromedriver')
     # context.driver = webdriver.Chrome(service=service)
-    # context.driver.maximize_window()
+    ############################################
 
-    # context.driver.implicitly_wait(4)
-    # driver_path = GeckoDriverManager().install()
-    # service = Service(driver_path)
-    # context.driver = webdriver.Firefox(service=service)
-    # context.driver = webdriver.Safari()
-
-    ### HEADLESS MODE ####
-    driver_path = ChromeDriverManager().install()
-    service = Service(driver_path)
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    context.driver = webdriver.Chrome(
-        options=options,
-        service=service
-    )
+    ######## HEADLESS MODE ########################
     # driver_path = ChromeDriverManager().install()
     # service = Service(driver_path)
-    # chrome_options = webdriver.ChromeOptions()
-    # options = ChromeOptions()
-    # chrome_options.add_argument("--headless")  # Run in headless mode without a GUI
-    # chrome_options.add_argument("--disable-gpu")  # Disable GPU acceleration (optional)
-    # context.driver = webdriver.Chrome(
-    #     options=options,
-    #     service=service
-    # )
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('--headless')
+    # options.add_argument('--window-size=1920,1080')
+    # context.driver = webdriver.Chrome(options=options,service=service)
+    ###############################################
 
+    ##################### FIREFOX #########################
+    # driver_path = GeckoDriverManager().install()
+    # service = FirefoxService(driver_path)
+    # context.driver = webdriver.Firefox(service=service)
+    ########################################################
+
+    ###################### BROWSERSTACK######################
+    options = FirefoxOptions()
+    bs_user = 'bolanleoso_tj7eAn'
+    bs_key = 'sYVfdEQpAKsppEKyy5qK'
+
+    # Setting the capabilities
+    caps = {
+        "os": "OS X",
+        "osVersion": "Ventura",
+        "sessionName": test_name
+    }
+
+    options.set_capability('bstack:options', caps)
+    options.set_capability('browserVersion', '95')
+    options.set_capability('browserName', 'Firefox')
+
+    # connecting the test to Browserstack
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    context.driver = webdriver.Remote(
+        command_executor=url,
+        options=options)
+    #################################################################
+
+    context.driver.maximize_window()
+    context.driver.implicitly_wait(5)
     context.driver.wait = WebDriverWait(context.driver, 10)
-
     context.app = Application(context.driver)
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
-
+    logger.info(f'Started scenario: {scenario.name}')
+    browser_init(context, scenario.name)
 
 def before_step(context, step):
     print('\nStarted step: ', step)
+    logger.info(f'Started step: {step}')
 
 
 def after_step(context, step):
     if step.status == 'failed':
         print('\nStep failed: ', step)
+        logger.error(f'Step failed: {step}')
 
+        # Mark test case as FAILED on BrowserStack:
+        # Documentation: https://www.browserstack.com/docs/automate/selenium/view-test-results/mark-tests-as-pass-fail
+        context.driver.execute_script(
+            'browserstack_executor: {"action": "setSessionStatus", "arguments": '
+            '{"status":"failed", "reason": "Step failed"}}'
+        )
 
 def after_scenario(context, feature):
     context.driver.delete_all_cookies()
     context.driver.quit()
+
+
--- pages/base_page.py
+++ pages/base_page.py
@@ -1,16 +1,18 @@
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# from support.logger import logger
 
 class Page:
 
     def __init__(self, driver):
         self.driver = driver
         self.wait = WebDriverWait(self.driver, 15)
-        self.base_url = 'https://shop.cureskin.com/'
+        self.base_url =  'https://shop.cureskin.com/'
 
     def open_url(self, url=''):
         print(f'Opening URL: {url}')
+        # logger.info(f'Opening URL: {url}')
         self.driver.get(url)
 
     def find_element(self, *locator):
@@ -20,6 +22,7 @@ class Page:
         return self.driver.find_elements(*locator)
 
     def click(self, *locator):
+        # logger.info('Clicking on {}, {}'.format(*locator))
         self.driver.find_element(*locator).click()
 
     def input_text(self, text, *locator):
@@ -27,17 +30,17 @@ class Page:
         e.clear()
         e.send_keys(text)
         print(f'Inputting text: {text}')
-
+        # logger.info(f'Inputting text: {text}')
 
     def wait_for_element_click(self, *locator):
-        e = self.wait.until(EC.element_to_be_clickable(locator), message=f'Element not clickable by {locator}')
+        e = self.wait.until(EC.element_to_be_clickable(*locator), message=f'Element not clickable by {locator}')
         e.click()
 
     def wait_for_element_disappear(self, *locator):
-        self.wait.until(EC.invisibility_of_element(locator))
+        self.wait.until(EC.invisibility_of_element(*locator))
 
     def wait_for_element_appear(self, *locator):
-        return self.wait.until(EC.presence_of_element_located(locator))
+        return self.wait.until(EC.presence_of_element_located(*locator))
 
     def verify_element_text(self, expected_text, *locator):
         actual_text = self.driver.find_element(*locator).text
