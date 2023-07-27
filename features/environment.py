0a54fd5640b1d45064fbb63d7b5ea64a542c7d9a
bola2044 <priscilla9313@gmail.com>
2023-07-27 15:28:42 -0400

    Modified environment.py

--- features/environment.py
+++ features/environment.py
@@ -2,9 +2,13 @@ import allure
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.wait import WebDriverWait
-# from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 from allure_commons.types import AttachmentType
-# from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.firefox.service import Service as FirefoxService
 
 from app.application import Application
 from support.logger import logger
@@ -24,14 +28,14 @@ def browser_init(context, test_name):
     :param test_name: scenario.name
     """
     ####### GOOGLE CHROME ######################
-    service = Service()
-    options = webdriver.ChromeOptions()
-    context.driver = webdriver.Chrome(service=service, options=options)
+    # service = Service()
+    # options = webdriver.ChromeOptions()
+    # context.driver = webdriver.Chrome(service=service, options=options)
     ############################################
 
     ######## HEADLESS MODE ########################
     # driver_path = ChromeDriverManager().install()
-    # service = Service(driver_path)
+    # service = Service(executable_path=r'C/Users/priscillao/QA/Cureskin/chromedriver')
     # options = webdriver.ChromeOptions()
     # options.add_argument('--headless')
     # options.add_argument('--window-size=1920,1080')
@@ -40,16 +44,17 @@ def browser_init(context, test_name):
 
     ##################### FIREFOX #########################
     # driver_path = GeckoDriverManager().install()
-    # service = FirefoxService(driver_path)
-    # context.driver = webdriver.Firefox(service=service)
+    # service = FirefoxService(executable_path='/Users/priscillao/QA/Cureskin/geckodriver')
+    # options = webdriver.FirefoxOptions()
+    # context.driver = webdriver.Firefox(service=service,options=options)
     ########################################################
 
     ###################### BROWSERSTACK######################
     # options = ChromeOptions()
     # bs_user = 'bolanleoso_tj7eAn'
     # bs_key = 'sYVfdEQpAKsppEKyy5qK'
-
-    # Setting the capabilities
+    #
+    # # Setting the capabilities
     # caps = {
     #     "os": "OS X",
     #     "osVersion": "Ventura",
@@ -59,13 +64,25 @@ def browser_init(context, test_name):
     # options.set_capability('bstack:options', caps)
     # options.set_capability('browserVersion', '95')
     # options.set_capability('browserName', 'Chrome')
-    # #
-    # # # connecting the test to Browserstack
+    #
+    # # connecting the test to Browserstack
     # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
     # context.driver = webdriver.Remote(
     #     command_executor=url,
     #     options=options)
     #################################################################
+
+
+   ##################### MOBILE EMULATION ############################
+    service = Service()
+    options = webdriver.ChromeOptions()
+    mobile_emulation = { "deviceName": "Nexus 5X" }
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
+    context.driver = webdriver.Chrome(service=service,options=options)
+    context.driver.get('https://shop.cureskin.com/')
+
+##################################################################################
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(5)
